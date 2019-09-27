578ca6bae2debd2ea9f7c99c39ad904e521bed8b
Leonid Shagabutdinov <leonid@shagabutdinov.com>
2019-09-27 16:45:20 +0300

    automatic commit on caldera at 2019-09-27 16:45:20+03:00

--- user/.config/puma.rb
+++ user/.config/puma.rb
@@ -1,6 +1,6 @@
-# workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-# threads_count = Integer(ENV['MAX_THREADS'] || 5)
-# threads threads_count, threads_count
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 2)
+threads threads_count, threads_count
 
 # # preload_app!
 
